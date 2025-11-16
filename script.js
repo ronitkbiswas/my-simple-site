3a17ac137cc0f9d6fcdc7f82f689ecd4b9b38c5b
Ronit <ronitkbiswas@gmail.com>
2025-11-16 11:17:24 +0530

    update title text

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const btn = document.getElementById('btn');
 const title = document.getElementById('title');
 
 btn.addEventListener('click', () => {
-  title.textContent = 'You clicked it — nice!';
+  title.textContent = 'ololo — nice!';
   alert('Good job — you just ran JS on a page.');
 });
